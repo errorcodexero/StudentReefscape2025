0cceb445e7452cbe1297dc0d74eef343d6ae2f82
pranay <kollup1@wvhs.wlwv.k12.or.us>
2025-12-16 20:16:15 -0800

    pushed ManipulatorIOHardware with errors

--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorConstants.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorConstants.java
@@ -1,5 +1,175 @@
 package frc.robot.subsystems.manipulator;
 
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.MomentOfInertia;
+import edu.wpi.first.units.measure.Time;
+
 public class ManipulatorConstants {
-    
+
+    public class Arm {
+       
+        // motor CAN ID
+        public static final int kMotorCANID = 2; 
+
+        // if motor is inverted 
+        public static final boolean kInverted = true; 
+
+        public static final double kGearRatio = 36.0 ;
+
+        public static final String kCANBusName = null; 
+
+        public static final Current kCurrentLimit = Amps.of(40); 
+
+        public static final Angle kPosTolerance = Degrees.of(1);
+        public static final AngularVelocity kVelTolerance = DegreesPerSecond.of(2);
+
+        public static final Time kCurrentLimitTime = Seconds.of(1); 
+
+        // The minimum and maximum arm angle, used to set the limits of travel
+        public static final Angle kMaxArmAngle = Degrees.of(179.0);
+        public static final Angle kMinArmAngle = Degrees.of(-180) ;
+
+
+        // Moment of intertia for the arm, used only for simulation
+        public static final MomentOfInertia kMOI = KilogramSquareMeters.of(0.001) ;
+
+        // The starting angle of the ARM as seen by the absolute encoder
+        public static final Angle kStartAbsEncoderAngle = Degrees.of(-45.0) ;        
+
+        public class PID {
+            public static final double kP = 4.0 ; 
+            public static final double kI = 0.0 ;
+            public static final double kD = 0.0 ;
+            public static final double kV = 0.1 ;
+            public static final double kA = 0.0 ;
+            public static final double kG = 0.0 ;
+            public static final double kS = 0.0 ;
+        }
+
+        public class MotionMagic {
+            // public static final AngularVelocity kMaxVelocity = RotationsPerSecond.of(32.0) ;
+            public static final AngularVelocity kMaxVelocity = RotationsPerSecond.of(32.0) ;
+            public static final AngularAcceleration kMaxAcceleration = RotationsPerSecondPerSecond.of(300.0) ;
+            public static final double kJerk = 3000.0 ;
+        }
+
+        public class ThruBoreEncoder {
+            // the encoder mapper needs double values, so these constants don't use the Units library
+            // robot min and max are in degrees
+            public static final double kRobotMax = 180; 
+            public static final double kRobotMin = -180; 
+            public static final double kEncoderMax = 0; 
+            public static final double kEncoderMin = 1; 
+            public static final double kRobotCalibrationValue = 0; 
+            public static final double kEncoderCalibrationValue = 0.515;
+
+            public static final int kEncoderSource = 3; 
+        }
+
+        public class Positions {
+            public static final Angle kStow = Degrees.of(0);
+            public static final Angle kPlaceL1 = Degrees.of(0); 
+            public static final Angle kPlaceL2 = Degrees.of(35); 
+            public static final Angle kPlaceL3 = Degrees.of(35); 
+            public static final Angle kPlaceL4 = Degrees.of(75);
+            public static final Angle kKickbackAngle = Degrees.of(-10); 
+            public static final Angle kCollect = Degrees.of(14.0) ;
+            public static final Angle kReefCollect = Degrees.of(-160.0) ;
+            public static final Angle kScoreAlgaeReef = Degrees.of(-160.0) ;
+            public static final Angle kRaiseAngle = Degrees.of(10.0) ;
+        }
+    }
+
+    public class Elevator {
+
+        // motor CAN ID
+        public static final int kMotorFrontCANID = 3;
+        public static final int kMotorBackCANID = 4;
+
+        // if motor is inverted 
+        public static final boolean kInverted = true;
+
+        // meters that elevator moves per revolution of motor
+        public static final double kMetersPerRev = 1.0 / 42.375  ;
+
+        public static final String kCANBusName = "" ;
+
+        public static final Current kCurrentLimit = Amps.of(40); 
+
+        public static final Distance kPosTolerance = Centimeter.of(0.5) ;
+        public static final LinearVelocity kVelTolerance = MetersPerSecond.of(0.01) ;
+
+        public static final Time kCurrentLimitTime = Seconds.of(1); 
+
+        // Gear ratio between the motor and the wheel that the cable wraps around       
+        // Used for simulation
+        public static final double kGearRatio = 36.0 ;
+
+        // The maximum height of the elevator
+        public static final Distance kMaxHeight = Centimeters.of(177.0) ;
+
+        // The minimum height of the elevator
+        public static final Distance kMinHeight = Centimeters.of(0.0) ;
+
+        // The MOI of the elevator, used only for simulation
+        public static final MomentOfInertia kMOI = KilogramSquareMeters.of(0.1) ;
+
+        public class PID {
+            public static final double kP = 20.0; 
+            public static final double kI = 0.0 ;
+            public static final double kD = 0.0 ;
+            public static final double kV = 0.15 ;
+            public static final double kA = 0.0 ;
+            public static final double kG = 0.288 ;
+            public static final double kS = 0.0 ;
+        }
+
+        public class MotionMagic {
+            public static final AngularVelocity kMaxVelocity = RotationsPerSecond.of(50) ;
+            public static final AngularAcceleration kMaxAcceleration = RotationsPerSecondPerSecond.of(300) ;
+            public static final double kJerk = 0.0 ;
+        }
+
+        public class Positions {
+            public static final Distance kStow = Meters.of(0);
+            public static final Distance kPlaceL1 = Centimeters.of(0); 
+            public static final Distance kPlaceL2 = Centimeters.of(27); 
+            public static final Distance kPlaceL3 = Centimeters.of(67); 
+            public static final Distance kPlaceL4 = Centimeters.of(130); 
+            public static final Distance kCollect = Centimeters.of(0.0) ;
+            public static final Distance kReefCollect = Centimeters.of(92.0) ;
+            public static final Distance kReefCollectL3 = Centimeters.of(92.0) ;
+            public static final Distance kScoreAlgaeReef = Centimeters.of(8.0) ;
+        }
+    }
+
+    public class Keepout {
+        public static final Distance kKeepoutHeight = Meters.of(0.4); 
+        public static final Angle kKeepoutMinAngle = Degrees.of(15.0) ;
+        public static final Angle kKeepoutMaxAngle = Degrees.of(90.0) ;
+    }
+
+    public class Positions {
+        public static final Distance kStowedHeight = Meters.of(0.0);
+        public static final Angle kStowedAngle = Degrees.of(0.0);
+
+        public static final Distance kCollectHeight = Meters.of(0.0);
+        public static final Angle kCollectAngle = Degrees.of(0.0);
+
+        public static final Distance kLowScoreHeight = Meters.of(0.0);
+        public static final Angle kLowScoreAngle = Degrees.of(0.0);
+
+        public static final Distance kHighScoreHeight = Meters.of(0.0);
+        public static final Angle kHighScoreAngle = Degrees.of(0.0);
+
+        public static final Distance kEjectAlgaeHeight = Centimeters.of(1.0);
+        public static final Angle kEjectAlgaeAngle = Degrees.of(10.0);
+    }
 }
--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorIOHardware.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorIOHardware.java
@@ -1,19 +1,122 @@
 package frc.robot.subsystems.manipulator;
 import static edu.wpi.first.units.Units.*;
+
+import org.xerosw.util.TalonFXFactory;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.DutyCycleEncoderSim;
 
 
 public class ManipulatorIOHardware implements ManipulatorIO  {
-    private TalonFX arm_motor_;
-    private TalonFX elevator_motor;
-    private TalonFX elevator_motor_2; 
-    private DutyCycleEncoder encoder_;
 
-    private DCMotorSim arm_Sim;
-    private DCMotorSim elevator_Sim;
-    private DutyCycleEncoderSim arm_encoder_sim;
-    private boolean encoder_motor_Synced;
+    private StatusSignal <Angle> elevator_pos_sig; 
+    private StatusSignal <AngularVelocity> elevator_vel_sig;
+    private StatusSignal <Voltage> elevator_1_vol_sig; 
+    private StatusSignal <Current> elevator_1_current_sig;
+    
+    private StatusSignal <Voltage> elevator_2_vol_sig; 
+    private StatusSignal <Current> elevator_2_current_sig_; 
+    
+    private Voltage elevator_voltage_;
+    
+    private final Debouncer armErrorDebounce_= new Debouncer(0.5);
+    private final Debouncer elevator1ErrorDebounce_= new Debouncer(0.5);
+    private final Debouncer elevator2ErrorDebounce_= new Debouncer(0.5);
+    
+    
+    public ManipulatorIOHardware() throws Exception {
+        //createArm() ;
+                createElevator() ; 
+                
+                
+                //sets the final update frequency to the motors 
+                TalonFXFactory.checkError(-2,"set manipulator frequnecy", () ->
+                BaseStatusSignal.setUpdateFrequencyForAll(
+                50.0, 
+                elevator_pos_sig,
+                elevator_vel_sig,
+                elevator_1_current_sig,
+                elevator_2_vol_sig,
+                elevator_2_current_sig_
+                )
+                );
+                
+            }
+            private void createArm() {
+                
+            }
+            private void createElevator() throws Exception {
+        elevator_motor_= TalonFXFactory.createTalonFX(
+        ManipulatorConstants.Elevator.kMotorFrontCANID,
+        ManipulatorConstants.Elevator.kCANBusName,
+        ManipulatorConstants.Elevator.kInverted,
+        ManipulatorConstants.Elevator.kCurrentLimit,
+        ManipulatorConstants.Elevator.kCurrentLimitTime
+        );
+        elevator_motor_.setPosition(Degrees.of(0)); 
+        
+        elevator_motor_2= TalonFXFactory.createTalonFX(
+        ManipulatorConstants.Elevator.kMotorBackCANID,
+        ManipulatorConstants.Elevator.kCANBusName,
+        ManipulatorConstants.Elevator.kInverted,
+        ManipulatorConstants.Elevator.kCurrentLimit,
+        ManipulatorConstants.Elevator.kCurrentLimitTime
+        );
+        elevator_motor_2.setControl(new Follower(ManipulatorConstants.Elevator.kMotorFrontCANID, false));
+        
+        //configurations for the elevator 
+        Slot0Configs elevator_pids = new Slot0Configs();
+        elevator_pids.kP = ManipulatorConstants.Elevator.PID.kP;
+        elevator_pids.kI = ManipulatorConstants.Elevator.PID.kI;
+        elevator_pids.kD = ManipulatorConstants.Elevator.PID.kD;
+        elevator_pids.kV = ManipulatorConstants.Elevator.PID.kV;
+        elevator_pids.kA = ManipulatorConstants.Elevator.PID.kA;
+        elevator_pids.kG = ManipulatorConstants.Elevator.PID.kG;
+        elevator_pids.kS = ManipulatorConstants.Elevator.PID.kS;
+        
+        MotionMagicConfigs elevatorMotionMagicConfigs = new MotionMagicConfigs();
+        elevatorMotionMagicConfigs.MotionMagicCruiseVelocity = ManipulatorConstants.Elevator.MotionMagic.kMaxVelocity.in(RotationsPerSecond) ;
+        elevatorMotionMagicConfigs.MotionMagicAcceleration = ManipulatorConstants.Elevator.MotionMagic.kMaxAcceleration.in(RotationsPerSecondPerSecond) ;
+        elevatorMotionMagicConfigs.MotionMagicJerk = ManipulatorConstants.Elevator.MotionMagic.kJerk;
+         
+        private 
+        
+            {
+        }
+    }
+            // updates all of the inputs from manipulatorIO 
+            @Override 
+            public class ManipulatorIO {
+            public void updateInputs(ManipulatorIOInputs inputs) {
+ 
+            }
+    }
 }
+    
+
+                
+                
+                
+                
+                
